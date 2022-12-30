e5130eefb64667f77864950de01404010ab93f6a
unknown <oasis@LAPTOP-NPA63NIP.mit.edu>
2022-12-29 19:49:50 -0500

    Finished part 1 of day 11

--- day11.py
+++ day11.py
@@ -14,30 +14,50 @@ def day11(file):
             self.operation = operation[0]
             self.tests = int(tests[0])
             self.true = int(true[0])
+            self.inspected_times = 0
             self.false = int(false[0])
 
-        def throw_item(self, other_monkey: Monkey):
+        def throw_item(self, item, other_monkey: Monkey):
             """
             Throws an item to another monkey
+            :param item: int
             :param other_monkey: Monkey
             :return: None
             """
+            other_monkey.receive_item(item)
 
-        def inspect_items(self, monkeys: dict[Monkey]):
+        def receive_item(self, item):
+            self.starting_items.append(item)
+
+        def inspect_items(self, monkeys: dict[int]):
             """
             The function that simulates how a monkey will inspect all elements
             :return:
             """
             for item in self.starting_items:
+                self.inspected_times += 1
                 operator = re.findall(r'\*|\+', self.operation)[0]
                 operand = re.split(r'\s\+\s|\s\*\s', self.operation)
                 term1 = item if operand[0] == 'old' else int(operand[0])
                 term2 = item if operand[1] == 'old' else int(operand[1])
 
                 if operator == "+":
-                    pass
-                elif operator == "*":
-                    pass
+                    new_worry_level = term1 + term2
+                else:
+                    new_worry_level = term1 * term2
+
+                new_worry_level //= 3
+                if new_worry_level % self.tests == 0:
+                    # we do the true
+                    self.throw_item(new_worry_level, monkeys[self.true])
+                else:
+                    # we do the false
+                    self.throw_item(new_worry_level, monkeys[self.false])
+
+            self.starting_items = []
+
+        def __str__(self):
+            return f"Monkey {self.number}: {' '.join([str(item) for item in self.starting_items])}"
 
     class Simulation:
         def __init__(self):
@@ -58,7 +78,7 @@ def day11(file):
             tests = re.findall(r'[0-9]+', stats[3])
             true = re.findall(r'[0-9]+', stats[4])
             false = re.findall(r'[0-9]+', stats[5])
-            self.monkeys[number[0]] = Monkey(number, starting_items, operation, tests, true, false)
+            self.monkeys[int(number[0])] = Monkey(number, starting_items, operation, tests, true, false)
 
         def start_round(self):
             """
@@ -69,6 +89,16 @@ def day11(file):
                 for monkey in self.monkeys:
                     self.monkeys[monkey].inspect_items(self.monkeys)
 
+        def top_two_annoying_monkey_scores(self):
+            """
+            Returns the result of multiplying two monkeys with the most inspected items
+            :return:
+            """
+            scores = [monkey.inspected_times for monkey in self.monkeys.values()]
+            scores.sort(reverse=True)
+
+            return scores[0] * scores[1]
+
     simulation = Simulation()
     lines = [line.strip('\n') for line in file.readlines() if line.strip('\n') != '']
 
@@ -76,6 +106,8 @@ def day11(file):
         simulation.add_monkey(lines[6 * monkey: 6 * (monkey + 1)])
 
     simulation.start_round()
+
+    return simulation.top_two_annoying_monkey_scores()
 
 
 if __name__ == '__main__':
