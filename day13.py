b8d4b81fc1ae59e2d7c572ce44531a4314291614
unknown <oasis@LAPTOP-NPA63NIP.mit.edu>
2023-01-01 16:21:37 -0500

    Finished part 2 of day 13
    Modified the is_right_order to allow cmp_to_key comparisons

--- day13.py
+++ day13.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
+from functools import cmp_to_key
 
 
-def is_right_order(left: list, right: list) -> bool:
+def is_right_order(left: list, right: list) -> int:
     """
     Determines if the left and right packet are in right order
     :param left: list
     :param right: list
-    :return: bool
+    :return: int
     """
     max_length = min(len(left), len(right))
 
@@ -17,13 +18,13 @@ def is_right_order(left: list, right: list) -> bool:
 
         if isinstance(left_val, int) and isinstance(right_val, int):
             if right_val < left_val:
-                return False
+                return 1
             elif right_val > left_val:
-                return True
+                return -1
 
         elif isinstance(left_val, list) and isinstance(right_val, list):
             is_ordered = is_right_order(left_val, right_val)
-            if is_ordered is not None:
+            if is_ordered != 0:
                 return is_ordered
         else:
             if isinstance(left_val, int):
@@ -31,11 +32,13 @@ def is_right_order(left: list, right: list) -> bool:
             elif isinstance(right_val, int):
                 right_val = [right_val]
             is_ordered = is_right_order(left_val, right_val)
-            if is_ordered is not None:
+            if is_ordered != 0:
                 return is_ordered
 
     if len(left) != len(right):
-        return len(left) < len(right)
+        return -1 if len(left) < len(right) else 1
+
+    return 0
 
 
 def parse_list(input_string: str, starting_index: int) -> tuple[list, int]:
@@ -91,10 +94,17 @@ def day13(file):
         left, right = left_packets[packet], right_packets[packet]
 
         is_ordered = is_right_order(left, right)
-        if is_ordered or is_ordered is None:
+
+        if is_ordered == -1 or is_ordered == 0:
             part1 += packet + 1
 
-    return part1
+    new_packets = left_packets + right_packets + [[2]] + [[6]]
+
+    new_packets.sort(key=cmp_to_key(is_right_order))
+
+    for packet in new_packets:
+        print(packet)
+    return part1, (new_packets.index([2]) + 1) * (new_packets.index([6]) + 1)
 
 
 if __name__ == '__main__':
